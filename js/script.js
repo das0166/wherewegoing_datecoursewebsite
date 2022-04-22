fa1b56fced7c77f195c05b4486ade699e26f79a2
das0166 <das0166@naver.com>
2022-04-23 00:08:09 +0900

    [04.23] 도시별 테마 80% 완성

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready( function() {
+    $(".course-select-box h3").click(function(){
+        $(".course-select-box h3, .course-select-box ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
 
-	
     
 
 
