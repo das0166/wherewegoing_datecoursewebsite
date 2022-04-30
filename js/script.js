75ba80d66b03937cadcda68da891d1a1688fdfc4
das0166 <das0166@naver.com>
2022-04-30 23:37:17 +0900

    [2022-04-30]테마별 장소 완성

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ $(document).ready( function() {
         $(this).addClass("on");
         $(this).next("ul").addClass("on");
     });
+    $(".theme .theme_list>div").click(function(){
+        $(".theme .theme_list>div, .theme .theme_list ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
 
     
 
