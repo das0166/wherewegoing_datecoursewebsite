6b95b321f9e36bb50381d401d05384bd3647158a
das0166 <das0166@naver.com>
2022-05-14 18:43:09 +0900

    [2022-05-14]city click 수정과 themepostmore modal 추가

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 $(document).ready( function() {
     $(".course-select-box h3").click(function(){
-        $(".course-select-box h3, .course-select-box ul").removeClass("on");
+        $(".course-select-box h3, .course-select-box .check, .course-select-box ul").removeClass("on");
         $(this).addClass("on");
-        $(this).next("ul").addClass("on");
+        $(this).next("div").addClass("on");
+        $(".on").next("ul").addClass("on");
     });
     $(".theme .theme_list>div").click(function(){
         $(".theme .theme_list>div, .theme .theme_list ul").removeClass("on");
         $(this).addClass("on");
         $(this).next("ul").addClass("on");
     });
-
+    $(".review_all .review").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+    $(".btn").click(function(){
+        $(".modal").fadeOut();
+    });
     
-
-
-
 });
